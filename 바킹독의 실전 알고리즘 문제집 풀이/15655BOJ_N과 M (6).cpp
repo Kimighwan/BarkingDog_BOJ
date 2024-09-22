d4e72954d5f5a0adabc42ccc9651fcbb7300053a
Kimikhwan <moonfgg@naver.com>
2024-09-22 19:57:03 +0900

    9.22

---
+++ 바킹독의 실전 알고리즘 문제집 풀이/15655BOJ_N과 M (6).cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+int arr[10];
+int ans[10];
+bool isUsed[10];
+
+void Solve(int cur) {
+	if (cur == m) {
+		for (int i = 0; i < m; i++)
+			cout << arr[ans[i]] << ' ';
+		cout << '\n';
+		return;
+	}
+
+	int start = 0;
+	if (cur != 0) start = arr[cur - 1] + 1;   
+	for (int i = start; i <= n; i++)
+	{
+		if (!isUsed[i])
+		{
+			ans[cur] = i;
+			isUsed[i] = true;
+			Solve(cur + 1);
+			isUsed[i] = false;
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+
+	sort(arr, arr + n);
+	Solve(0);
+}
