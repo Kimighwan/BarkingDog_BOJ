4d02cdbc7ff29602ff10bf28eba13d4f8583de8d
Kimikhwan <moonfgg@naver.com>
2024-09-29 15:29:58 +0900

    9.29

---
+++ 바킹독의 실전 알고리즘 문제집 풀이/16987BOJ_계란으로 계란치기_노이해.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int n, mx = 0;
+int s[10], w[10];
+int cnt = 0; // 현재 깨진 계란 수
+
+void Solve(int index) // index 번째 계란으로 깰 차례
+{
+	if (index == n) {
+		mx = max(mx, cnt);
+		return;
+	}
+
+	if (s[index] <= 0 || cnt == n - 1) {
+		Solve(index + 1);
+		return;
+	}
+
+	for (int i = 0; i < n; i++) {
+		if (index == i || s[i] <= 0) continue;
+		s[index] -= w[i];
+		s[i] -= w[index];
+		if (s[index] <= 0) cnt++;
+		if (s[i] <= 0) cnt++;
+		Solve(index + 1);
+		if (s[index] <= 0) cnt--;
+		if (s[i] <= 0) cnt--;
+		s[i] += w[index];
+		s[index] += w[index];
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie();
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+		cin >> s[i] >> w[i];
+
+	Solve(0);
+	cout << mx;
+}
