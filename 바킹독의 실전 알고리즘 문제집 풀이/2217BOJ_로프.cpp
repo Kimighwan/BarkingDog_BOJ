2fb8045e51135f0f4a238627d16a7d05177d339f
Kimikhwan <moonfgg@naver.com>
2024-10-10 21:15:54 +0900

    10.10

---
+++ 바킹독의 실전 알고리즘 문제집 풀이/2217BOJ_로프.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, w[100'005];
+int total = 0;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+		cin >> w[i];
+
+	sort(w + 1, w + n + 1, greater<>());
+
+	for (int k = 1; k <= n; k++) {
+		total = max(total, k * w[k]);
+	}
+
+	cout << total;
+}
