0a03c23147180bc09d7515d4a58d465b732b35c3
Kimikhwan <moonfgg@naver.com>
2024-10-19 18:21:07 +0900

    10.19

---
+++ 바킹독의 실전 알고리즘 문제집 풀이/18870BOJ_좌표 압축.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n;
+int arr[1000001];
+vector<int> v;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+		v.emplace_back(arr[i]);
+	}
+
+	sort(v.begin(), v.end());
+	v.erase(unique(v.begin(), v.end()), v.end());
+	for (int i = 0; i < n; i++)
+		cout << lower_bound(v.begin(), v.end(), arr[i]) - v.begin() << " ";
+}
