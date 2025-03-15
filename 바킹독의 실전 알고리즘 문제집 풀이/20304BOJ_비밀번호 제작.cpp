710c990a18de93a3ae787047bd32d5acc50ecb21
Kimikhwan <moonfgg@naver.com>
2025-03-15 18:12:46 +0900

    3.15

--- 바킹독의 실전 알고리즘 문제집 풀이/20304BOJ_비밀번호 제작.cpp
+++ 바킹독의 실전 알고리즘 문제집 풀이/20304BOJ_비밀번호 제작.cpp
@@ -1,20 +1,53 @@
 #include <iostream>
-
+#include <algorithm>
+#include <vector>
+#include <queue>
 using namespace std;
 
-int n, m;
-int pass[1'000'000];
+int dist[1'000'010];
 
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 
+	int n, m, tmp, next;
+
 	cin >> n;
 	cin >> m;
+	vector<int> start{};
 	for (int i = 0; i < m; i++)
 	{
-		cin >> pass[i];
+		cin >> tmp;
+		start.push_back(tmp);
 	}
 
+	fill(dist, dist + n + 2, -1);
 
+	queue<int> q{};
+	for (auto& item : start) {
+		dist[item] = 0;
+		q.push(item);
+	}
+
+	while (!q.empty()) {
+		auto cur = q.front();
+		q.pop();
+
+		for (int k{}; k < 31; ++k) {
+			if (cur & (1 << k))
+				next = cur & ~(1 << k);
+			else
+				next = cur | (1 << k);
+			if (next > n) continue;
+			if (dist[next] >= 0) continue;
+			dist[next] = dist[cur] + 1;
+			q.push(next);
+		}
+	}
+
+	int ans = 0;
+	for (int i{}; i <= n; ++i)
+		ans = max(ans, dist[i]);
+
+	cout << ans;
 }
--- 바킹독의 실전 알고리즘 문제집 풀이/6549BOJ_미해결.cpp
+++ 바킹독의 실전 알고리즘 문제집 풀이/6549BOJ_미해결.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <stack>
+
+// 높이가 작아지면 최소 높이 체크하고 최소 높이 기준이로 너비 비교
+// 높이가 커지면 해당 사각형 너비와 마지막 너비 구한 높이 기준으로 새로운 사각형을 포함한 너비로 비교
+
 using namespace std;
 int main() {
 	ios::ios_base::sync_with_stdio(0);
@@ -10,11 +14,11 @@ int main() {
 		int n;
 		cin >> n;
 		if (n == 0) break;
-		stack<pair<long long, long long>> s;
+		stack < pair <long long, long long>> s;	// 높이, 처음 등장하는 index
 		long long result = 0;
-		long long h;
 		for (int i = 0; i < n; i++)
 		{
+			int h;
 			cin >> h;
 			int index = i;
 			while (!s.empty() && s.top().first >= h)
@@ -24,10 +28,5 @@ int main() {
 				s.pop();
 			}
 			s.push({ h, i });
-			while (!s.empty())
-			{
-				result = s.top().first * ()
-			}
-		}
 	}
 }
