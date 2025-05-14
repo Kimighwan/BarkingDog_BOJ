02b205ddc16a70a7b48ccaebcb5ee80b5d1f4e44
Kimikhwan <moonfgg@naver.com>
2025-05-14 10:54:37 +0900

    5.14 같은 수로 계속 나누어 합성수가 존재하지 않는 다는 점에서 소수만을 뽑아내고 가장 작은 약수는 n 제곱근 이하이므로 i * i <= n 범위까지 확인해준다

--- 바킹독의 실전 알고리즘 문제집 풀이/11653BOJ_소인수분해.cpp
+++ 바킹독의 실전 알고리즘 문제집 풀이/11653BOJ_소인수분해.cpp
@@ -1,16 +1,33 @@
+//#include <iostream>
+//using namespace std;
+//
+//int n;
+//
+//int main() {
+//	ios::ios_base::sync_with_stdio(0);
+//	cin.tie(0);
+//
+//	cin >> n;
+//	for (int i = 2; i * i <= n; i++)
+//	{
+//		while (n % i == 0) {
+//			cout << i << " ";
+//			n /= i;
+//		}
+//	}
+//	if (n != 1) cout << n;
+//}
+
 #include <iostream>
 using namespace std;
 
-int n;
-
 int main() {
-	ios::ios_base::sync_with_stdio(0);
-	cin.tie(0);
+	int n; cin >> n;
 
-	cin >> n;
 	for (int i = 2; i * i <= n; i++)
 	{
-		while (n % i == 0) {
+		while (n % i == 0)
+		{
 			cout << i << " ";
 			n /= i;
 		}
