4630f7e4d755041d4b45c755631e83bf22447c0f
Kimikhwan <moonfgg@naver.com>
2025-05-19 13:35:16 +0900

    5.19 조합의 공식과 개념에 대해서 알면 풀기 쉬운 문제다

--- 바킹독의 실전 알고리즘 문제집 풀이/11051BOJ_이항 계수 2.cpp
+++ 바킹독의 실전 알고리즘 문제집 풀이/11051BOJ_이항 계수 2.cpp
@@ -1,19 +1,38 @@
+//#include <iostream>
+//using namespace std;
+//
+//int n, k;
+//int dp[1001][1001]; // iCj
+//
+//int main() {
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//
+//	cin >> n >> k;
+//
+//	for (int i = 1; i <= n; i++) {
+//		dp[i][0] = dp[i][i] = 1;
+//		for (int j = 1; j < i; j++)
+//			dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % 10007;
+//	}
+//	cout << dp[n][k];
+//}
+
 #include <iostream>
 using namespace std;
 
+int dp[1001][1001]; // nCk
 int n, k;
-int dp[1001][1001]; // iCj
 
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-
 	cin >> n >> k;
 
-	for (int i = 1; i <= n; i++) {
+	for (int i = 1; i <= n; i++)
+	{
 		dp[i][0] = dp[i][i] = 1;
 		for (int j = 1; j < i; j++)
 			dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % 10007;
 	}
+
 	cout << dp[n][k];
 }
